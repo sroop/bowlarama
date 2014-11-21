56322884810306bc55ea5cf3a2f50e153cea5600
Sroop Sunar <sroopsunar@hotmail.com>
2014-11-21 12:15:25 +0000

    Users must have a present and unique username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
    has_and_belongs_to_many :games
+  validates :username, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
    it { is_expected.to have_and_belong_to_many(:games) }
+   it { is_expected.to validate_presence_of(:username) }
+   it { is_expected.to validate_uniqueness_of(:username) }
 end
