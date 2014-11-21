0945ef3142cf8bf54d78171757442afbe1751d98
Sroop Sunar <sroopsunar@hotmail.com>
2014-11-21 12:09:49 +0000

    Amend shoulda matchers to reflect rspec.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
-  it { should have_and_belong_to_many(:users) }
+  it { is_expected.to have_and_belong_to_many(:users) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-   it { should have_and_belong_to_many(:games) }
+   it { is_expected.to have_and_belong_to_many(:games) }
 end
